cd3a6f80ce81bab5f635ed8d2ee290183bc9cb8a
Atemu <atemu.main@gmail.com>
2019-12-17 19:47:27 +0000

    Set hostname to KYKLOPS

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "KYKLOPS";
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "32ECBB16"; # crc32 of 'KYKLOPS'
 
