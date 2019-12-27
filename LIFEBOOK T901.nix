5d8a73253bb0ab039ebe399f69a60b2634dd911c
Atemu <atemu.main@gmail.com>
2019-12-27 07:00:24 +0100

    Add LIFEBOOK T901.nix
    It's the hardware-configuration.nix that nixos-generate-config auto-generates on the machine

---
+++ LIFEBOOK T901.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
+
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nix.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+}
