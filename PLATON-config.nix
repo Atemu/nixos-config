db7bdffb63d4cb34ede2d73882d05cc171af695b
Atemu <atemu.main@gmail.com>
2019-12-27 21:18:25 +0100

    PLATON: Add a local DNSCrypt-proxy 2 resolver
    NixOS doesn't offer a DNSCrypt-proxy 2 module yet,
    so doing it via a Docker image is the next best thing I could think of

--- PLATON-config.nix
+++ PLATON-config.nix
@@ -59,6 +59,14 @@
   networking.networkmanager.wifi.scanRandMacAddress = true; # default
   networking.networkmanager.unmanaged = [ "docker0" "virbr0" ];
 
+  # Enable dnscrypt-proxy-2 via a Docker image
+  # FIXME: Use the native Nix dnscrypt-proxy-2 module when it's available
+  docker-containers.dnscrypt-proxy = {
+    image = "atemu12/dnscrypt-proxy-2";
+    ports = [ "53:53/udp" ];
+    volumes = [ "dnscrypt-blacklist:/blacklist/:ro" ];
+  };
+
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
