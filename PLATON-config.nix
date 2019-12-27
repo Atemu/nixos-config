8dbfe44f0e76eec98a1ae2f7c50184c66bdb3507
Atemu <atemu.main@gmail.com>
2019-12-27 19:30:35 +0100

    PLATON: Enable BSPWM
    My preferred WM/DE

--- PLATON-config.nix
+++ PLATON-config.nix
@@ -81,6 +81,10 @@
   services.xserver.enable = true;
   services.xserver.displayManager.startx.enable = true;
 
+  services.xserver.windowManager.bspwm.enable = true;
+  services.xserver.desktopManager.default = "none+bspwm";
+
+
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
 
