c17fcfa9d60031d1340bcb6f9e05b0592173bfd1
Atemu <atemu.main@gmail.com>
2020-01-13 08:28:15 +0100

    PLATON: Import Zpool from /dev/mapper, not /dev/disk/by-id
    The DM devices in the default dir are dm-uuid-{LVM,CRYPT}-ultralonggarbagestring
    when using stacked blockdevs which looks ugly
    Now it should show $VGNAME-$LVNAME

--- PLATON-storage.nix
+++ PLATON-storage.nix
@@ -15,6 +15,7 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportAll = false;
   boot.zfs.forceImportRoot = false;
+  boot.zfs.devNodes = "/dev/mapper/";
 
   fileSystems."/" = {
     device = "Ppool/P/root";
