a5d8bbac90cd0ebd2a347b314ecf2648abee6784
Atemu <atemu.main@gmail.com>
2020-01-31 17:27:11 +0100

    Desktop: Refactor out desktop specific networking
    It makes sense to have that common between all desktops too, I want
    NetworkManager on every machine that isn't a server

--- PLATON-config.nix
+++ PLATON-config.nix
@@ -14,23 +14,7 @@
 
   programs.mosh.enable = true;
 
-  networking.networkmanager.enable = true;
-
-  networking.networkmanager.logLevel = "INFO"; # Prints useful info to journalctl
-  networking.networkmanager.wifi.backend = "iwd";
-  networking.networkmanager.wifi.macAddress = "stable"; # TODO what exactly does this mean?
   networking.networkmanager.wifi.powersave = true;
-  networking.networkmanager.wifi.scanRandMacAddress = true; # default
-  networking.networkmanager.unmanaged = [ "docker0" "virbr0" ];
-
-  # Enable dnscrypt-proxy-2 via a Docker image
-  # FIXME: Use the native Nix dnscrypt-proxy-2 module when it's available
-  docker-containers.dnscrypt-proxy = {
-    image = "atemu12/dnscrypt-proxy-2";
-    ports = [ "53:53/udp" ];
-    volumes = [ "dnscrypt-blacklist:/blacklist/:ro" ];
-  };
-  networking.resolvconf.useLocalResolver = true;
 
   virtualisation.docker.enable = true;
   virtualisation.docker.storageDriver = "zfs";
