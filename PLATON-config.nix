b88f780abc99c5a463bffee26eb4762359a17434
Atemu <atemu.main@gmail.com>
2020-02-22 20:16:54 +0100

    PLATON: Disable undervolt timer
    It was causing spam in the log and running it after boot and resume is a
    much cleaner solution

--- PLATON-config.nix
+++ PLATON-config.nix
@@ -11,6 +11,14 @@
   services.tlp.enable = true;
 
   services.undervolt.enable = true;
+  # It don't want the logspam, this is only needed because undervolt resets on boot
+  systemd.timers.undervolt.enable = false;
+  # I don't exactly understand why but if undervolt needs to be both after and
+  # wanted by post-resume.target to be run on resume
+  # Being wanted by the multi-user.target makes undervolt run on boot and
+  # on generation switch in a live system
+  systemd.services.undervolt.wantedBy = [ "post-resume.target" "multi-user.target" ];
+  systemd.services.undervolt.after = [ "post-resume.target" ];
 
   services.logind.extraConfig = "HandlePowerKey=suspend";
 
