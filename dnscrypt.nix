1687b530b942a0064cdfbba88e21ad4ddda9c7d9
Atemu <atemu.main@gmail.com>
2020-03-03 08:31:43 +0100

    Dnscrypt: Use native NixOS dnscrypt-proxy2 module
    I use some trickery to enhance the upstream example config with my own
    settings, see https://github.com/NixOS/nixpkgs/pull/82584

--- dnscrypt.nix
+++ dnscrypt.nix
@@ -1,13 +1,33 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
-  # Enable dnscrypt-proxy-2 via a Docker image
-  # FIXME: Use the native Nix dnscrypt-proxy-2 module when it's available
-  docker-containers.dnscrypt-proxy = {
-    image = "atemu12/dnscrypt-proxy-2";
-    ports = [ "53:53/udp" ];
-    volumes = [ "dnscrypt-blacklist:/blacklist/:ro" ];
-  };
-  networking.resolvconf.useLocalResolver = true;
+  services.dnscrypt-proxy2.enable = true;
 
+  services.dnscrypt-proxy2.settings = {
+    ipv6_servers = true;
+    require_dnssec = true;
+    require_nolog = true;
+    require_nofilter = true;
+    lb_strategy = "p2";
+    lb_estimator = true;
+    dnscrypt_ephemeral_keys = true;
+    tls_disable_session_tickets = true;
+  };
+
+  services.dnscrypt-proxy2.configFile = pkgs.runCommand "dnscrypt-proxy.toml" {
+    json =
+      let
+        settings = builtins.fromJSON (
+          builtins.readFile (
+            pkgs.runCommand "example-dnscrypt-proxy.json" {} ''
+              ${pkgs.remarshal}/bin/toml2json ${pkgs.dnscrypt-proxy2.src}/dnscrypt-proxy/example-dnscrypt-proxy.toml > $out
+            ''
+          )
+        ) // config.services.dnscrypt-proxy2.settings;
+      in
+        builtins.toJSON settings;
+    passAsFile = [ "json" ];
+  } ''
+    ${pkgs.remarshal}/bin/json2toml < $jsonPath > $out
+  '';
 }
