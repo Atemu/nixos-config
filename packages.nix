54f6dedea94fb3d9f609c97d455bb86cd396aada
Atemu <atemu.main@gmail.com>
2020-03-25 19:42:34 +0100

    Packages: Fix indendation
    Idk why it was 4-wide before

--- packages.nix
+++ packages.nix
@@ -1,118 +1,119 @@
 pkgs: with pkgs;
-  {
-    # Packages to always install.
-    common = [
-      acpi
-      aespipe
-      bash-completion
-      bc
-      bind
-      brightnessctl
-      btrfs-progs
-      cifs-utils
-      clang # DOOM emacs
-      compsize
-      cryptsetup
-      curl
-      ddrescue
-      diceware
-      fd
-      ffmpeg-full
-      ffsend
-      file
-      git
-      gnupg
-      gotop
-      hdparm
-      iftop
-      iotop
-      iperf
-      iw
-      jq
-      killall
-      libarchive
-      linuxPackages.cpupower
-      linuxPackages.perf
-      lm_sensors
-      lsof
-      lz4
-      man-pages
-      mediainfo
-      mimic # Mycroft's TTS
-      modemmanager
-      mosh
-      ncdu
-      neofetch
-      netcat-gnu
-      nix-bash-completions
-      nmap
-      nox
-      ntfs3g
-      pciutils
-      pv
-      ranger
-      rclone
-      ripgrep
-      rsync
-      rtv
-      screen
-      shellcheck
-      silver-searcher
-      smartmontools
-      stress
-      sysstat
-      testdisk
-      thefuck
-      traceroute
-      tree
-      unzip
-      usbutils
-      vim
-      wget
-      which
-      whois
-      wol
-      zip
-      zstd
-    ];
 
-    # Packages to install if X is not enabled.
-    noX = [
-      emacs26-nox
-      rxvt_unicode.terminfo
-    ];
+{
+  # Packages to always install.
+  common = [
+    acpi
+    aespipe
+    bash-completion
+    bc
+    bind
+    brightnessctl
+    btrfs-progs
+    cifs-utils
+    clang # DOOM emacs
+    compsize
+    cryptsetup
+    curl
+    ddrescue
+    diceware
+    fd
+    ffmpeg-full
+    ffsend
+    file
+    git
+    gnupg
+    gotop
+    hdparm
+    iftop
+    iotop
+    iperf
+    iw
+    jq
+    killall
+    libarchive
+    linuxPackages.cpupower
+    linuxPackages.perf
+    lm_sensors
+    lsof
+    lz4
+    man-pages
+    mediainfo
+    mimic # Mycroft's TTS
+    modemmanager
+    mosh
+    ncdu
+    neofetch
+    netcat-gnu
+    nix-bash-completions
+    nmap
+    nox
+    ntfs3g
+    pciutils
+    pv
+    ranger
+    rclone
+    ripgrep
+    rsync
+    rtv
+    screen
+    shellcheck
+    silver-searcher
+    smartmontools
+    stress
+    sysstat
+    testdisk
+    thefuck
+    traceroute
+    tree
+    unzip
+    usbutils
+    vim
+    wget
+    which
+    whois
+    wol
+    zip
+    zstd
+  ];
 
-    # Packages to install if X is enabled.
-    x = [
-      android-studio
-      anki
-      blender
-      chromium
-      dmenu
-      emacs
-      firefox-bin
-      geogebra
-      gimp
-      gnome3.adwaita-icon-theme # fix for lutris' icons FIXME 
-      jdk
-      jetbrains.idea-community
-      krita
-      libreoffice
-      lutris
-      lxrandr
-      mpv
-      networkmanagerapplet
-      riot-desktop
-      rxvt_unicode
-      signal-desktop
-      steam
-      sxhkd
-      tigervnc
-      virtmanager
-      vlc
-      xcape
-      xclip
-      xorg.xbacklight
-      xorg.xev
-    ];
-  }
+  # Packages to install if X is not enabled.
+  noX = [
+    emacs26-nox
+    rxvt_unicode.terminfo
+  ];
+
+  # Packages to install if X is enabled.
+  x = [
+    android-studio
+    anki
+    blender
+    chromium
+    dmenu
+    emacs
+    firefox-bin
+    geogebra
+    gimp
+    gnome3.adwaita-icon-theme # fix for lutris' icons FIXME 
+    jdk
+    jetbrains.idea-community
+    krita
+    libreoffice
+    lutris
+    lxrandr
+    mpv
+    networkmanagerapplet
+    riot-desktop
+    rxvt_unicode
+    signal-desktop
+    steam
+    sxhkd
+    tigervnc
+    virtmanager
+    vlc
+    xcape
+    xclip
+    xorg.xbacklight
+    xorg.xev
+  ];
+}
