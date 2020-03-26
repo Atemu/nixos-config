fb604cf6b14c0cb7206cd9c957c17a04105d4cf0
Atemu <atemu.main@gmail.com>
2020-03-26 18:55:11 +0100

    LELANTOS: Add fileSystems
    These come from an image I used to bootstrap the PI

--- LELANTOS-storage.nix
+++ LELANTOS-storage.nix
@@ -2,4 +2,13 @@
   # Use the extlinux bootloader instead of GRUB
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.grub.enable = false;
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2178-694E";
+      fsType = "vfat";
+    };
 }
