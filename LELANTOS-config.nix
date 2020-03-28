401bf0f5817f619b91f116d29d09db6294886248
Atemu <atemu.main@gmail.com>
2020-03-28 17:12:44 +0100

    LELANTOS: Open dnscrypt-proxy to the network
    LELANTOS shall act as a DNS proxy for my home network

--- LELANTOS-config.nix
+++ LELANTOS-config.nix
@@ -5,4 +5,6 @@
   security.sudo.wheelNeedsPassword = false;
 
   services.dnscrypt-proxy2.enable = true;
+  services.dnscrypt-proxy2.settings.listen_addresses = [ "0.0.0.0:53" ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
 }
