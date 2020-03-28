57f7c8817d28a5772809ebf5e7ed7305d1c0174b
Atemu <atemu.main@gmail.com>
2020-03-28 15:58:22 +0100

    LELANTOS: Add zramSwap
    Same config as on all the other systems but with memoryPercent = 100 because the
    RPI only has 1GiB
    
    I might even push it up to 200%, even 300% have been necessary for some builds
    in the past

--- LELANTOS-storage.nix
+++ LELANTOS-storage.nix
@@ -11,4 +11,13 @@
     { device = "/dev/disk/by-uuid/2178-694E";
       fsType = "vfat";
     };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "lz4";
+    memoryPercent = 100;
+    numDevices = 1; # default
+    priority = 5; # default
+    swapDevices = 1; # why do I need this?
+  };
 }
