d874aa45adad120946a4bca5d6d996a5a8598e04
Atemu <atemu.main@gmail.com>
2020-05-08 16:31:26 +0200

    RPI2B: Work around OOM during builds with overcommit
    This makes more of the 1GB zram swap available to processes

--- RASPBERRY PI 2 MODEL B.nix
+++ RASPBERRY PI 2 MODEL B.nix
@@ -8,6 +8,11 @@
   boot.initrd.availableKernelModules = [ ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
+  boot.kernel.sysctl = {
+    # Help with running OOM during builds
+    "vm.overcommit_memory" = 1; # always overcommit
+    "vm.overcommit_ratio" = 100; # how much of the total VM should be allocatable by processes
+  };
   boot.extraModulePackages = [ ];
 
   swapDevices = [ ];
