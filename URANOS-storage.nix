dd8eca409dc6fe2aa908508c61de57a0c2247bb9
Atemu <atemu.main@gmail.com>
2020-06-09 18:57:19 +0200

    URANOS: Enable GRUB with EFI

--- URANOS-storage.nix
+++ URANOS-storage.nix
@@ -1,5 +1,9 @@
 { ... }:
 {
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiSupport = true;
+
   boot.initrd.luks.devices = {
     Elements_25A3-crypt = {
       device = "/dev/disk/by-uuid/250918cc-965e-4873-80c9-7361ea622723";
