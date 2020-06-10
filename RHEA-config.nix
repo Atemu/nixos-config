c9a60ba988b10066954560944d9be3260fecce42
Atemu <atemu.main@gmail.com>
2020-06-10 07:15:47 +0200

    RHEA: Don't override passwords
    This is done in common.nix already now

--- RHEA-config.nix
+++ RHEA-config.nix
@@ -1,10 +1,6 @@
 { config, ... }:
 
 {
-  # TODO find a nicer way to set the same value for all users automatically
-  users.users.atemu.hashedPassword = "$6$66BjFoGY9Ckwnrr$xaj10.vNEBy4Rg5gCplROGuwZL6M3K4IQqLhEYW1ZnMMkBqm9VSENmVARmAJ.2a2YrDJeX79KSCag8.8rcV6e/";
-  users.users.root.hashedPassword = "$6$66BjFoGY9Ckwnrr$xaj10.vNEBy4Rg5gCplROGuwZL6M3K4IQqLhEYW1ZnMMkBqm9VSENmVARmAJ.2a2YrDJeX79KSCag8.8rcV6e/";
-
   services.openssh.enable = true;
 
   programs.mosh.enable = true;
