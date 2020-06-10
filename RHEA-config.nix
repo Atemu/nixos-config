088bebd722f478e577558b09e6f780fbc0ae560e
Atemu <atemu.main@gmail.com>
2020-06-10 07:14:15 +0200

    RHEA: Make non-desktop
    I think I'd prefer its purpose to be a USB drive I can stick into a machine to
    get SSH access

--- RHEA-config.nix
+++ RHEA-config.nix
@@ -1,10 +1,6 @@
 { config, ... }:
 
 {
-  imports = [
-    ./desktop.nix
-  ];
-
   # TODO find a nicer way to set the same value for all users automatically
   users.users.atemu.hashedPassword = "$6$66BjFoGY9Ckwnrr$xaj10.vNEBy4Rg5gCplROGuwZL6M3K4IQqLhEYW1ZnMMkBqm9VSENmVARmAJ.2a2YrDJeX79KSCag8.8rcV6e/";
   users.users.root.hashedPassword = "$6$66BjFoGY9Ckwnrr$xaj10.vNEBy4Rg5gCplROGuwZL6M3K4IQqLhEYW1ZnMMkBqm9VSENmVARmAJ.2a2YrDJeX79KSCag8.8rcV6e/";
