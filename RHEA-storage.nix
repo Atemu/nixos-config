82debcdddc37ef1f0e5fd085cae81ed1a120d52e
Atemu <atemu.main@gmail.com>
2020-06-10 07:40:12 +0200

    RHEA: Import ZFS from by-id
    No longer using device mapper as underlying storage

--- RHEA-storage.nix
+++ RHEA-storage.nix
@@ -8,7 +8,7 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportAll = false;
   boot.zfs.forceImportRoot = false;
-  boot.zfs.devNodes = "/dev/mapper/";
+  boot.zfs.devNodes = "/dev/disk/by-id/"; # default
 
   fileSystems."/" = {
     device = "Rpool/deployment/root";
