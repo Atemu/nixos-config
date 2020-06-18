576d08026fd805ceb52403ee899371423a4bba19
Atemu <atemu.main@gmail.com>
2020-06-19 00:22:51 +0200

    HEPHAISTOS: Apply fsync kernel patch
    This is a gaming machine and I play CPU intensive games (GW2)

--- HEPHAISTOS-config.nix
+++ HEPHAISTOS-config.nix
@@ -6,6 +6,16 @@
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_5_6;
+  boot.kernelPatches = [
+    {
+      name = "fsync";
+      patch = pkgs.fetchpatch {
+        name = "futex-wait-multiple-5.2.1.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/futex-wait-multiple-5.2.1.patch?h=linux-fsync&id=06b35b38cf9932ee3209e941a1e3a41c663f519b";
+        sha256 = "0z2wxqjcnsg6flcq1jk9wv8r8y2sm52iwv3lndpr1sizr3q5xr3q";
+      };
+    }
+  ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
