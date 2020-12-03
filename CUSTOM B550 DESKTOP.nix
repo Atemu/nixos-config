172cf4de64e99c4c2dbeda13d10327f86b351830
Atemu <atemu.main@gmail.com>
2020-12-03 02:16:10 +0100

    B550: Enable IOMMU

--- CUSTOM B550 DESKTOP.nix
+++ CUSTOM B550 DESKTOP.nix
@@ -8,6 +8,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelParams = [ "amd_iommu=on" ];
   boot.extraModulePackages = [ ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
