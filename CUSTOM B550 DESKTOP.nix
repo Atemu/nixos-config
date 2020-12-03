baf3876ee97c1a02bf304d1fb0b266410c8859b5
Atemu <atemu.main@gmail.com>
2020-12-03 02:02:30 +0100

    B550: Init
    Auto-generated.

---
+++ CUSTOM B550 DESKTOP.nix
@@ -0,0 +1,14 @@
+{ lib, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+}
