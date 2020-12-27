baf117207adef57aeb24b7e229d6fa05265bb7d1
Atemu <atemu.main@gmail.com>
2020-12-27 10:37:39 +0100

    HEPHAISTOS: Don't wait for networking

--- configs/HEPHAISTOS/default.nix
+++ configs/HEPHAISTOS/default.nix
@@ -35,6 +35,9 @@
   # Makes Docker socket activated, only starting it after I use it once
   systemd.services.docker.wantedBy = lib.mkForce [ ]; # TODO put in some sort of common module
 
+  # Don't wait for something as unreliable as networking before letting me use my system
+  systemd.targets.network-online.enable = false;
+
   programs.adb.enable = true;
 
   system.stateVersion = "20.09";
