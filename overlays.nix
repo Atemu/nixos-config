06be43876e0a81d4a283b287bbe1f2098d5f129e
Atemu <atemu.main@gmail.com>
2021-06-03 14:48:24 +0200

    Overlays: Unset DESKTOP_STARTUP_ID in jetbrains IDEs
    Needed to get https://github.com/fctorial/enter_nix_shell to work fully

--- overlays.nix
+++ overlays.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   nixpkgs.overlays = [
     (next: prev: {
@@ -17,6 +19,16 @@
         '';
         doCheck = false;
       });
+
+      jetbrains = let
+        mkUnset = pkg: pkg.overrideAttrs (old: {
+          postFixup = old.postFixup or "" + ''
+            # Needed for enter nix shell plugin to change the IDE's env fully
+            wrapProgram $out/bin/${pkg.meta.mainProgram} --unset DESKTOP_STARTUP_ID
+          '';
+        });
+        isJBIDE = pkg: lib.isDerivation pkg && !lib.hasPrefix "jetbrains-jdk" pkg.name;
+      in lib.mapAttrs (n: v: if isJBIDE v then mkUnset v else v) prev.jetbrains;
     })
   ];
 }
