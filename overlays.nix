b245f1a29a146528968148bd111d57e6bdb3ea7b
Atemu <atemu.main@gmail.com>
2021-06-09 19:47:39 +0200

    Overlays: next -> final
    That's the cannonical name for that argument

--- overlays.nix
+++ overlays.nix
@@ -2,14 +2,14 @@
 
 {
   nixpkgs.overlays = [
-    (next: prev: {
-      nix = next.nixUnstable.overrideAttrs (old: {
+    (final: prev: {
+      nix = final.nixUnstable.overrideAttrs (old: {
         postInstallCheck = ''
           mv $out/bin/nix $out/bin/nixUnstable
           ln $out/bin/nixUnstable $out/bin/nixFlakes
           for cmd in $out/bin/nix-* ; do ln -sf nixUnstable "$cmd" ; done
 
-          ln -s ${next.nixStable}/bin/nix $out/bin/nixStable
+          ln -s ${final.nixStable}/bin/nix $out/bin/nixStable
           ln -s $out/bin/nixStable $out/bin/nix
 
           mv $out/share/bash-completion/completions/nix $out/share/bash-completion/completions/nixUnstable
