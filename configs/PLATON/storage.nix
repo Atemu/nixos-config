28e156df2ec0740f8ffa9612c4c15103da6dfb9e
Atemu <atemu.main@gmail.com>
2021-06-21 22:39:12 +0200

    PLATON: New btrfs-based installation

--- configs/PLATON/storage.nix
+++ configs/PLATON/storage.nix
@@ -1,57 +1,55 @@
 # This file contains the configuration of disks and storage
 { config, ... }:
+
+let
+  rootPool = mkUuid "1700c195-e991-4c08-9055-5d0403fb1cc6"; # Device to mount the main btrfs pool from
+
+  # TODO put in custom lib
+  mkUuid = uuid: "/dev/disk/by-uuid/${uuid}";
+  mkMount = subvol: {
+    device = rootPool;
+    fsType = "btrfs";
+    options = [ "compress-force=zstd:1" "subvol=${subvol}" ];
+  };
+in
+
 {
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.efiSupport = true;
 
+  # TODO refactor into custom.luksPools
   boot.initrd.luks.devices = {
-    MZNTE128HMGR-crypt = {
-      device = "/dev/disk/by-uuid/6f9558c5-593c-4c86-b88f-63cc5b031ff0";
+    TR200_18PB71KWK3QS-crypt = {
+      device = mkUuid "642a307a-9b37-414b-b53b-045eeb277d72";
       allowDiscards = true;
     };
   };
 
-  custom.zfs.enable = true;
-  boot.zfs.devNodes = "/dev/mapper/";
+  fileSystems = {
+    "/tmp" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [ "size=50%" "nosuid" "nodev" "nodev" "mode=1777" ]; # systemd default security options
+    };
 
-  # Instance-specific
-  fileSystems."/" = {
-    device = "Ppool/instance/root";
-    fsType = "zfs";
+    "/boot" = {
+      device = mkUuid "B6F5-6DA6";
+      fsType = "vfat";
+      options = [ "umask=077" ];
+    };
+
+    "/" = mkMount "root";
+    "/nix" = mkMount "nix";
+    "/home" = mkMount "home";
+    "/var/opt/games" = mkMount "games";
   };
-  fileSystems."/tmp" = {
-    device = "tmpfs";
-    fsType = "tmpfs";
-    options = [ "size=50%" "nosuid" "nodev" "nodev" "mode=1777" ]; # systemd default security options
-  };
-  # Deployment-specific
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/3075-C917";
-    fsType = "vfat";
-    options = [ "umask=077" ];
-  };
-  fileSystems."/nix" = {
-    device = "Ppool/deployment/nix";
-    fsType = "zfs";
-  };
-  fileSystems."/var/lib/docker" = {
-    device = "Ppool/deployment/docker";
-    fsType = "zfs";
-  };
-  # Purpose-specific
-  fileSystems."/home" = {
-    device = "Ppool/purpose/home";
-    fsType = "zfs";
-  };
-  fileSystems."/etc/NetworkManager/system-connections" = {
-    device = "Ppool/purpose/nm-connections";
-    fsType = "zfs";
-  };
-  fileSystems."/var/opt/games" = {
-    device = "Ppool/purpose/games";
-    fsType = "zfs";
-  };
+
+  # TODO put in custom.btrfs
+  # Also, is this still necessary?
+  boot.initrd.availableKernelModules = [
+    "xxhash_generic" # needed to boot btrfs with xxhash64
+  ];
 
   custom.zramSwap.enable = true;
 }
