d123a5daf02469b3a834c135cba83d196c7e228f
Atemu <atemu.main@gmail.com>
2021-06-22 20:12:30 +0200

    Nix: Comment

--- nix.nix
+++ nix.nix
@@ -4,22 +4,30 @@ stdenv.mkDerivation {
   inherit (nixUnstable) pname version;
   src = nixUnstable;
   installPhase = ''
+    # Symlink-copy of nixUnstable
     cp -as $src $out
     chmod -R +rw $out
 
     mv $out/bin/nix $out/bin/nixUnstable
     ln -s nixUnstable $out/bin/nixFlakes
+
+    # All nix-* commands need to point at nixUnstable now (it's a multi-call binary)
     for cmd in $out/bin/nix-* ; do ln -sf nixUnstable "$cmd" ; done
 
+    # Provide old nix as nixStable
     ln -s ${nixStable}/bin/nix $out/bin/nixStable
+    # nix needs to be the old one for nix-bash-completions (and my muscle memory) to work
     ln -s nixStable $out/bin/nix
 
+    # Make nixUnstable's built-in bash-completions work for the new binary names
     cd $out/share/bash-completion/completions/
     cp nix nixUnstable
     cp nix nixFlakes
-    rm nix
+    # We need to replace the command names to be completed with the new ones
     substituteInPlace nixUnstable --replace " nix" " nixUnstable"
     substituteInPlace nixFlakes --replace " nix" " nixFlakes"
+    # Don't try to complete the nix binary anymore, it's old Nix now and would conflict nix-bash-completions
+    rm nix
     cd -
   '';
 
