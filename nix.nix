ba3a1946f94f192e8fa14d5e8e11faa95ca85850
Atemu <atemu.main@gmail.com>
2021-11-13 23:00:39 +0100

    nix: adjust for 2.4 release

--- nix.nix
+++ nix.nix
@@ -1,42 +1,42 @@
-{ stdenv, nixStable, nixUnstable, lib }:
+{ stdenv, nix_2_3, nixStable, lib }:
 
 stdenv.mkDerivation {
-  inherit (nixUnstable) pname version;
-  src = nixUnstable;
+  inherit (nixStable) pname version;
+  src = nixStable;
   installPhase = ''
-    # Symlink-copy of nixUnstable
+    # Symlink-copy of nixStable
     cp -as $src $out
     chmod -R +rw $out
 
     cd $out/bin
-    mv nix nixUnstable
-    ln -s nixUnstable nixFlakes
+    mv nix nixStable
+    ln -s nixStable nixFlakes
 
-    # All nix-* commands need to point at nixUnstable now (it's a multi-call binary)
-    for cmd in nix-* ; do ln -sf nixUnstable "$cmd" ; done
+    # All nix-* commands need to point at nixStable now (it's a multi-call binary)
+    for cmd in nix-* ; do ln -sf nixStable "$cmd" ; done
 
-    # Provide old nix as nixStable
-    ln -s ${nixStable}/bin/nix nixStable
+    # Provide old nix as nix_2_3
+    ln -s ${nix_2_3}/bin/nix nix_2_3
     # nix needs to be the old one for nix-bash-completions (and my muscle memory) to work
-    ln -s nixStable nix
+    ln -s nix_2_3 nix
     cd -
 
-    # Make nixUnstable's built-in bash-completions work for the new binary names
+    # Make nixStable's built-in bash-completions work for the new binary names
     cd $out/share/bash-completion/completions/
-    cp nix nixUnstable
+    cp nix nixStable
     cp nix nixFlakes
     # We need to replace the command names to be completed with the new ones
-    substituteInPlace nixUnstable --replace " nix" " nixUnstable"
+    substituteInPlace nixStable --replace " nix" " nixStable"
     substituteInPlace nixFlakes --replace " nix" " nixFlakes"
     # Don't try to complete the nix binary anymore, it's old Nix now and would conflict nix-bash-completions
     rm nix
     cd -
   '';
 
-  meta = nixUnstable.meta // {
-    description = "My custom nixStable nixUnstable hybrid";
+  meta = nixStable.meta // {
+    description = "My custom nix_2_3 nixStable hybrid";
     longDescription = ''
-      My custom Nix with the `nix` command from nixStable and everything else from nixUnstable.
+      My custom Nix with the `nix` command from nix_2_3 and everything else from nixStable.
 
       The new `nix` is available via `nixUnstble` or `nixFlakes`. Bash completions work for these.
 
