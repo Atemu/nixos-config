34824a7f216079aa8e15415f8f0d9c6cb5d97106
Atemu <atemu.main@gmail.com>
2022-01-21 18:01:09 +0100

    VM: Adjust for upstream VM detection changes
    https://github.com/NixOS/nixpkgs/pull/151082
    
    Should support stable, new unstable and older unstable.

--- vm.nix
+++ vm.nix
@@ -1,15 +1,27 @@
 { lib, config, ... }:
 
-with lib;
+let
+  hasVmVariant = config.virtualisation ? vmVariant;
+  vmConfig = {
+    users.users.atemu.initialHashedPassword = "";
+    users.users.root.initialHashedPassword = "";
 
-# Only applied when this is built via build-vm
-mkIf (builtins.hasAttr "vm" config.system.build) {
-  users.users.atemu.initialHashedPassword = "";
-  users.users.root.initialHashedPassword = "";
+    security.sudo.wheelNeedsPassword = lib.mkForce false;
 
-  security.sudo.wheelNeedsPassword = mkForce false;
+    services.mingetty.autologinUser = "atemu";
 
-  services.mingetty.autologinUser = "atemu";
+    custom.withPackages = false;
+  };
+in
 
-  custom.withPackages = false;
-}
+# Ugly because the module system doesn't support if then else.
+lib.mkMerge [
+  {
+    virtualisation.vmVariant = lib.mkIf hasVmVariant vmConfig;
+
+    # The mkIf is *false*, but the option checker doesn't care.
+    # Get your shit together module system. I should not have to do this.
+    _module.check = false; # Ew.
+  }
+  (lib.mkIf (!hasVmVariant && builtins.hasAttr "vm" config.system.build) vmConfig)
+]
