46686eae6463374201eb8162582c419144ff5d27
Atemu <atemu.main@gmail.com>
2022-02-20 19:34:48 +0100

    Lib: Add mkLabel

--- lib.nix
+++ lib.nix
@@ -4,5 +4,8 @@
   config.lib.custom = {
     # Makes a by-uuid device out of UUID
     mkUuid = uuid: "/dev/disk/by-uuid/${uuid}";
+
+    # Makes a by-label device out of label
+    mkLabel = label: "/dev/disk/by-label/${label}";
   };
 }
