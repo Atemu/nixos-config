40e722f0521889179fb26492af2b02f4ebcafe92
Atemu <atemu.main@gmail.com>
2022-02-27 19:20:34 +0100

    HEPHAISTOS: Switch to xanmod (5.15)
    5.16 has an AMDGPU regression that makes my display flicker
    
    Xanmod is on 5.15 and also has futex patches etc.

--- configs/HEPHAISTOS/default.nix
+++ configs/HEPHAISTOS/default.nix
@@ -13,7 +13,7 @@
 
   custom.desktop.enable = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_lqx;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
 
   services.xserver.videoDrivers = [ "amdgpu" ];
   services.xserver.deviceSection = ''
