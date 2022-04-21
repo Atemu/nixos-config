3817bd6d8e04f2882b0bbfff86bdacfad1979046
Atemu <atemu.main@gmail.com>
2022-04-21 20:28:24 +0200

    SOTERIA: Migrate to label-based disk handling

--- configs/SOTERIA/storage.nix
+++ configs/SOTERIA/storage.nix
@@ -5,35 +5,26 @@ with config.lib.custom;
 
 {
   custom.fs.enable = true;
-  custom.fs.boot = mkUuid "21F5-7652";
+  custom.fs.boot = mkLabel "SOTERIA";
   custom.fs.btrfs.enable = true;
-  custom.fs.btrfs.device = mkUuid "53a12e2b-fdd8-4b83-ba37-369baa7ec1ab";
+  custom.fs.btrfs.device = mkLabel "SOTERIA-root";
 
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.efiSupport = true;
 
   boot.initrd.luks.devices = {
-    SAMSUNG_MZ7LN256HCHP-crypt = {
-      device = mkUuid "82a2b716-11ca-4c0b-8c4f-5470b8a97bd9";
+    SOTERIA-crypt01 = {
+      device = mkLabel "SOTERIA-crypt01";
       allowDiscards = true;
     };
-
-    TRION100-crypt = {
-      device = "/dev/disk/by-uuid/140c8ebf-0664-47d6-8c71-cf9ef655efe0";
+    SOTERIA-crypt02 = {
+      device = mkLabel "SOTERIA-crypt02";
       allowDiscards = true;
     };
-    WD10EADS-crypt = {
-      device = "/dev/disk/by-uuid/90fcb44c-275a-4641-99c6-9c36b753f283";
-    };
-    WD10EZEX-crypt = {
-      device = "/dev/disk/by-uuid/41c3bb59-d732-4e53-99c5-4ab346f143b9";
-    };
-    ST1000DM003-crypt = {
-      device = "/dev/disk/by-uuid/b22bbe9d-3513-4d93-81ee-6ce60da0bab1";
-    };
-    ST2000DM006-crypt = {
-      device = "/dev/disk/by-uuid/e9b4abff-8f8d-4bd1-a366-1938a89c12bf";
+    SOTERIA-crypt03 = {
+      device = mkLabel "SOTERIA-crypt03";
+      allowDiscards = true;
     };
   };
 }
