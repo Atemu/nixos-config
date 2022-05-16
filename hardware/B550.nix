0d736eae1912b2ce0ed427a80d857337c943626c
Atemu <atemu.main@gmail.com>
2022-05-16 19:28:46 +0200

    B550: Backport amdgpu suspend fix
    Should be in 5.15 soon but isn't yet

--- hardware/B550.nix
+++ hardware/B550.nix
@@ -16,13 +16,23 @@
   boot.kernelParams = [ "amd_iommu=on" ];
   boot.extraModulePackages = [ ];
 
-  boot.kernelPatches = lib.mkIf (config.boot.kernelPackages.kernel.kernelOlder "5.11") [ {
-    name = "nct6687";
-    patch = pkgs.fetchpatch {
-      url = "https://patchwork.kernel.org/project/linux-hwmon/patch/20201202025057.5492-1-andareed@gmail.com/mbox/";
-      sha256 = "02mhxzm7zjv6bnwfjg4m7n4cv1cplhx6vwg8lynd4l1x6h2mzwa8";
-    };
-  } ];
+  boot.kernelPatches = [
+    (lib.mkIf (lib.versions.majorMinor config.boot.kernelPackages.kernel.version == "5.15") {
+      name = "amdgpu suspend fix";
+      patch = pkgs.fetchpatch {
+        url = "https://patchwork.freedesktop.org/patch/485708/raw/";
+        sha256 = "sha256-1HWWfBDulZ0ewAt50BNfm9IgOcqO0QYyHFneeTRjkJc=";
+      };
+    })
+
+    (lib.mkIf (config.boot.kernelPackages.kernel.kernelOlder "5.11") {
+      name = "nct6687";
+      patch = pkgs.fetchpatch {
+        url = "https://patchwork.kernel.org/project/linux-hwmon/patch/20201202025057.5492-1-andareed@gmail.com/mbox/";
+        sha256 = "02mhxzm7zjv6bnwfjg4m7n4cv1cplhx6vwg8lynd4l1x6h2mzwa8";
+      };
+    })
+  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
 
