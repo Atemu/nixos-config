34b3b5cae06e273269adf1270f88162f5e9d247a
Atemu <atemu.main@gmail.com>
2022-11-11 18:04:04 +0100

    URANOS: Enable btrbk auto-snapshots

--- configs/URANOS/storage.nix
+++ configs/URANOS/storage.nix
@@ -12,4 +12,24 @@ with config.lib.custom;
   custom.fs.boot = mkUuid "21F5-7652";
   custom.fs.btrfs.enable = true;
   custom.fs.btrfs.device = mkUuid "53a12e2b-fdd8-4b83-ba37-369baa7ec1ab";
+
+  # systemd.timers.btrbk-btrbk.timerConfig.AccuracySec = lib.mkForce "1s";
+  services.btrbk.instances."btrbk" = {
+    onCalendar = "*:0/15"; # Every 15min
+    settings = {
+      timestamp_format = "long-iso"; # Most sensible of the bunch
+      snapshot_preserve = "12h 7d 3w 1m 0y"; # Don't want to keep too many snapshots
+      # snapshot_preserve_min = "3h 3d 1w 1m 0y";
+      snapshot_preserve_min = "1h"; # Preserve all my <1h snapshots
+
+      preserve_day_of_week = "monday"; # A week starts on monday.
+
+      snapshot_create = "onchange"; # Only create snapshots if the subvol actually changed
+      snapshot_dir = "Snapshots";
+
+      volume."/System/Volumes" = {
+        subvolume."Users" = { };
+      };
+    };
+  };
 }
