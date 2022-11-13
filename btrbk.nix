64d4990b7eb5671ca4e93cb188537d2abd55268e
Atemu <atemu.main@gmail.com>
2022-11-13 13:25:19 +0100

    Btrbk: Extract btrbk config into module

--- btrbk.nix
+++ btrbk.nix
@@ -1,3 +1,35 @@
-{
+{ config, lib, ... }:
 
+let
+  inherit (lib) mkEnableOption mkOption mkIf;
+
+  cfg = config.custom.btrbk;
+in
+
+{
+  options.custom.btrbk = {
+    enable = mkEnableOption "automatic snapshots using btrbk";
+
+    volume = mkOption {
+      description = "Btrbk volumes declarations";
+      default = { };
+    };
+  };
+
+  config.services.btrbk.instances."btrbk" = mkIf cfg.enable {
+    onCalendar = "*:0/15"; # Every 15min
+    settings = {
+      timestamp_format = "long-iso"; # Most sensible of the bunch
+      snapshot_preserve = "12h 7d 3w 1m 0y"; # Don't want to keep too many snapshots
+      # snapshot_preserve_min = "3h 3d 1w 1m 0y";
+      snapshot_preserve_min = "1h"; # Preserve all my <1h snapshots
+
+      preserve_day_of_week = "monday"; # A week starts on monday.
+
+      snapshot_create = "onchange"; # Only create snapshots if the subvol actually changed
+      snapshot_dir = "Snapshots";
+
+      inherit (cfg) volume;
+    };
+  };
 }
--- configs/URANOS/storage.nix
+++ configs/URANOS/storage.nix
@@ -13,23 +13,8 @@ with config.lib.custom;
   custom.fs.btrfs.device = mkUuid "53a12e2b-fdd8-4b83-ba37-369baa7ec1ab";
   custom.fs.btrfs.newLayout = true;
 
-  # systemd.timers.btrbk-btrbk.timerConfig.AccuracySec = lib.mkForce "1s";
-  services.btrbk.instances."btrbk" = {
-    onCalendar = "*:0/15"; # Every 15min
-    settings = {
-      timestamp_format = "long-iso"; # Most sensible of the bunch
-      snapshot_preserve = "12h 7d 3w 1m 0y"; # Don't want to keep too many snapshots
-      # snapshot_preserve_min = "3h 3d 1w 1m 0y";
-      snapshot_preserve_min = "1h"; # Preserve all my <1h snapshots
-
-      preserve_day_of_week = "monday"; # A week starts on monday.
-
-      snapshot_create = "onchange"; # Only create snapshots if the subvol actually changed
-      snapshot_dir = "Snapshots";
-
-      volume."/System/Volumes" = {
-        subvolume."Users" = { };
-      };
-    };
+  custom.btrbk.enable = true;
+  custom.btrbk.volume."/System/Volumes" = {
+    subvolume."Users" = { };
   };
 }
