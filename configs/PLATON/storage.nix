bd7069667973e7d73f5492d2820ebc784c8cb57d
Atemu <atemu.main@gmail.com>
2023-05-05 19:28:12 +0200

    PLATON: Migrate to custom.luks and custom.fs

--- configs/PLATON/storage.nix
+++ configs/PLATON/storage.nix
@@ -1,49 +1,15 @@
 # This file contains the configuration of disks and storage
 { config, ... }:
 
-let
-  rootPool = mkUuid "1700c195-e991-4c08-9055-5d0403fb1cc6"; # Device to mount the main btrfs pool from
-
-  # TODO put in custom lib
-  mkUuid = uuid: "/dev/disk/by-uuid/${uuid}";
-  mkMount = subvol: {
-    device = rootPool;
-    fsType = "btrfs";
-    options = [ "compress-force=zstd:1" "subvol=${subvol}" ];
-  };
-in
-
 {
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.efiSupport = true;
 
-  # TODO refactor into custom.luksPools
-  boot.initrd.luks.devices = {
-    TR200_18PB71KWK3QS-crypt = {
-      device = mkUuid "642a307a-9b37-414b-b53b-045eeb277d72";
-      allowDiscards = true;
-    };
-  };
+  custom.luks.autoDevices = 1;
 
-  fileSystems = {
-    "/tmp" = {
-      device = "tmpfs";
-      fsType = "tmpfs";
-      options = [ "size=50%" "nosuid" "nodev" "nodev" "mode=1777" ]; # systemd default security options
-    };
-
-    "/boot" = {
-      device = mkUuid "B6F5-6DA6";
-      fsType = "vfat";
-      options = [ "umask=077" ];
-    };
-
-    "/" = mkMount "root";
-    "/nix" = mkMount "nix";
-    "/home" = mkMount "home";
-    "/var/opt/games" = mkMount "games";
-  };
+  custom.fs.enable = true;
+  custom.fs.btrfs.enable = true;
 
   # TODO put in custom.btrfs
   # Also, is this still necessary?
