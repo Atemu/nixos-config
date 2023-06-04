d45a37c3aff17f828bfe7525ba41cb96ddf1e435
Atemu <atemu.main@gmail.com>
2023-06-04 18:22:16 +0200

    URANOS: faster boot time (doesn't work)

--- configs/URANOS/default.nix
+++ configs/URANOS/default.nix
@@ -17,5 +17,24 @@
 
   custom.vm.enable = true;
 
+  # Libvirt takes forever to start, socket activate it when I actually need it
+  systemd.services.libvirtd.wantedBy = lib.mkForce [ ];
+  systemd.services.libvirtd-config.wantedBy = lib.mkForce [ ];
+  systemd.services.mount-pstore.wantedBy = lib.mkForce [ ];
+
+  systemd.defaultUnit = "default-online.target";
+  systemd.targets.default-online.wants = [ "multi-user.target" "network-online.target" ];
+
+  # Be required by graphical-online rather than multi-user.
+  systemd.targets.network-online.wantedBy = lib.mkForce [ "graphical-online.target" ];
+
+  systemd.services.tailscaled.wantedBy = lib.mkForce [ ];
+
+  # Don't need this feature.
+  systemd.services.libvirt-guests.wantedBy = lib.mkForce [ ];
+
+  # Why is this in the multi-user.target? Only network-online should care.
+  systemd.services.dhcpcd.wantedBy = lib.mkForce [ "network-online.target" ];
+
   system.stateVersion = "22.11";
 }
