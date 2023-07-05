3aaef384ae1e1ec967e99caba040e90bd0ecd3ab
Atemu <atemu.main@gmail.com>
2023-07-05 15:22:51 +0200

    SOTERIA: Use systemd-boot

--- configs/SOTERIA/storage.nix
+++ configs/SOTERIA/storage.nix
@@ -7,9 +7,11 @@ with config.lib.custom;
   custom.fs.enable = true;
   custom.fs.btrfs.enable = true;
 
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.efiSupport = true;
+  # TODO externalise
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "auto";
+  boot.loader.efi.canTouchEfiVariables = true;
 
   custom.luks.autoDevices = 3;
 }
