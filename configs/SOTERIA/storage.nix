e31a1a90306196a050df225c59b1323ab15d82fc
Atemu <atemu.main@gmail.com>
2023-07-05 15:48:51 +0200

    SOTERIA: Mount data volume

--- configs/SOTERIA/storage.nix
+++ configs/SOTERIA/storage.nix
@@ -15,4 +15,12 @@ with config.lib.custom;
   custom.fs.enable = true;
   custom.fs.btrfs.enable = true;
   custom.fs.btrfs.newLayout = true;
+
+  custom.btrfs.fileSystems = {
+    # TODO refactor fs.nix such that custom.btrfs can be configured for multiple volumes
+    "/Volumes/Data" = {
+      subvol = "/";
+      device = mkLabel "${config.custom.hostName}-data";
+    };
+  };
 }
