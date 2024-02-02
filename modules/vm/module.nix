292fa514cdd8757d1bb2141f1be6751e8aed6fd2
Atemu <atemu.main@gmail.com>
2024-02-02 23:03:41 +0100

    vm: "cfg" -> "this"

--- modules/vm/module.nix
+++ modules/vm/module.nix
@@ -1,14 +1,14 @@
 { lib, config, ... }:
 
 let
-  cfg = config.custom.vm;
+  this = config.custom.vm;
   inherit (lib) mkEnableOption mkMerge mkIf;
 in
 
 {
   options.custom.vm.enable = mkEnableOption "sensible defaults for use in a VM for testing";
 
-  config = mkIf cfg.enable {
+  config = mkIf this.enable {
     users.users.atemu.initialHashedPassword = "";
     users.users.root.initialHashedPassword = "";
 
