41d1eecca48f12acf6dbdf47947f97bd2229a0f3
Atemu <atemu.main@gmail.com>
2024-02-04 20:14:19 +0100

    immich: generate env file

--- modules/immich/module.nix
+++ modules/immich/module.nix
@@ -20,11 +20,34 @@ in
           url = "https://github.com/immich-app/immich/releases/download/v1.93.3/docker-compose.yml";
           hash = "sha256-EpEDmLI7VTxzg8opyyrvyYqKnF8b6D7NxtEQupdPu7g=";
         };
+
+        env = pkgs.writeText "env" (lib.generators.toKeyValue { } {
+          # You can find documentation for all the supported env variables at https://immich.app/docs/install/environment-variables
+
+          # The location where your uploaded files are stored
+          # TODO do a proper one
+          UPLOAD_LOCATION = "/var/tmp/immich/";
+
+          # The Immich version to use. You can pin this to a specific version like "v1.71.0"
+          IMMICH_VERSION = "release";
+
+          # Connection secret for postgres. You should change it to a random password
+          DB_PASSWORD = "postgres";
+
+          # The values below this line do not need to be changed
+          ###################################################################################
+          DB_HOSTNAME = "immich_postgres";
+          DB_USERNAME = "postgres";
+          DB_DATABASE_NAME = "immich";
+
+          REDIS_HOSTNAME = "immich_redis";
+        });
+
       in
         pkgs.runCommand "immich-docker" { } ''
           mkdir -p $out
           ln -s ${yml} $out/docker-compose.yml
-          ln -s ${./.env} $out/.env
+          ln -s ${env} $out/.env
         '';
     };
 
