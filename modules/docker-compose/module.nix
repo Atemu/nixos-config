0f07164fb18684a6ff0f556e94ed0002b9fcc267
Atemu <atemu.main@gmail.com>
2024-02-05 09:29:45 +0100

    docker-compose: add directory option
    Allows using providing a directory with docker-compose.yml in it

--- modules/docker-compose/module.nix
+++ modules/docker-compose/module.nix
@@ -4,17 +4,31 @@ let
   this = config.custom.docker-compose;
 
   inherit (lib) mkOption mkIf mapAttrs' nameValuePair;
-  inherit (lib.types) attrsOf submodule path;
+  inherit (lib.types) attrsOf submodule nullOr path;
 in
 
 {
   options.custom.docker-compose = mkOption {
     default = { };
-    type = attrsOf (submodule ({ name, ... }: {
+    type = attrsOf (submodule ({ name, config, ... }: {
       options = {
-        file = mkOption {
-          default = throw "You must provide a file for the ${name} docker-compose service";
+        directory = mkOption {
+          default = (
+            if config.file != null then
+              pkgs.runCommand "docker-compose-${name}" { } ''
+                mkdir -p $out/
+                ln -s ${config.file} $out/docker-compose.yml
+              ''
+            else
+              throw "You must provide a docker-compose.yml (or directory containing it) for the ${name} docker-compose service."
+          );
+          defaultText = "{option}`file if it is set.";
           type = path;
+        };
+        file = mkOption {
+          default = null;
+          type = nullOr path;
+          description = "The path to a docker-compose.yml.";
         };
       };
     }));
@@ -26,7 +40,7 @@ in
     systemd.services = mapAttrs' (name: value:
       nameValuePair "docker-compose-${name}" {
         serviceConfig = let
-          run = command: "${lib.getExe pkgs.docker} compose -f ${value.file} ${command}";
+          run = command: "${lib.getExe pkgs.docker} compose -f ${value.directory}/docker-compose.yml ${command}";
         in {
           # Stop services before in case they're running
           ExecStartPre = run "down";
