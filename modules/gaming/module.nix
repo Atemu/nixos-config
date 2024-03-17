dc35acc8fed509d2496f24a98798c4bf9028ec9c
Atemu <atemu.main@gmail.com>
2024-03-17 11:28:05 +0100

    gaming: enable gamemode
    Sets affinity automatically

--- modules/gaming/module.nix
+++ modules/gaming/module.nix
@@ -69,6 +69,8 @@ in
       "teamspeak-client"
     ];
 
+    programs.gamemode.enable = true;
+
     boot.kernel.sysctl = {
       # SteamOS/Fedora default, can help with performance.
       "vm.max_map_count" = 2147483642;
