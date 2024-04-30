8b9108566921fa8f013f44f08c0649870dc762e9
Atemu <atemu.main@gmail.com>
2024-04-30 15:42:14 +0200

    SOTERIA: share a zpool over my network with NFS

--- configs/SOTERIA/storage.nix
+++ configs/SOTERIA/storage.nix
@@ -12,6 +12,21 @@ with config.lib.custom;
   custom.fs.btrfs.enable = true;
   custom.fs.btrfs.newLayout = true;
 
+  # The zpool is managed imperatively and the network address is hard-coded for now
+  # as this is a temporary setup
+  boot.supportedFilesystems = [
+    "zfs"
+  ];
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /share/media 192.168.178.0/24(ro)
+  '';
+  networking.firewall = {
+    # It needs one or some of these ports AFAICT
+    allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+    allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
+  };
+
   custom.btrfs.fileSystems = {
     # TODO refactor fs.nix such that custom.btrfs can be configured for multiple volumes
     "/Volumes/Data" = {
