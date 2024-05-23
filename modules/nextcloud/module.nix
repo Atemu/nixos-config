847b2dc0ede16f86bd5b4caf746de91ecbf3e34c
Atemu <atemu.main@gmail.com>
2024-05-23 13:14:40 +0200

    nextcloud: init

---
+++ modules/nextcloud/module.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  this = config.custom.nextcloud;
+in
+
+{
+  options.custom.nextcloud = {
+    enable = lib.mkEnableOption "my custom nextcloud config";
+  };
+
+  imports = [
+    (lib.mkAliasOptionModule
+      [
+        "custom"
+        "nextcloud"
+        "virtualHost"
+      ]
+      [
+        "custom"
+        "virtualHosts"
+        "nextcloud"
+      ]
+    )
+  ];
+
+  config = lib.mkIf this.enable {
+    services.nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud28;
+      hostName = config.custom.virtualHosts.nextcloud.domain;
+      https = true;
+      config = {
+        adminpassFile = toString (pkgs.writeText "password" "none");
+      };
+    };
+
+    custom.virtualHosts.nextcloud = {
+      onlyEnableTLS = true;
+    };
+  };
+}
