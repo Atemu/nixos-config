41a056516a5d5f53e1cdc61108b5dabf76770a94
Atemu <atemu.main@gmail.com>
2024-05-24 13:48:24 +0200

    nextcloud: add collabora code support

--- modules/nextcloud/module.nix
+++ modules/nextcloud/module.nix
@@ -7,11 +7,13 @@
 
 let
   this = config.custom.nextcloud;
+  ncDomain = config.custom.virtualHosts.nextcloud.domain;
 in
 
 {
   options.custom.nextcloud = {
     enable = lib.mkEnableOption "my custom nextcloud config";
+    code.enable = lib.mkEnableOption "collabora CODE via Docker";
   };
 
   imports = [
@@ -33,7 +35,7 @@ in
     services.nextcloud = {
       enable = true;
       package = pkgs.nextcloud28;
-      hostName = config.custom.virtualHosts.nextcloud.domain;
+      hostName = ncDomain;
       https = true;
       config = {
         adminpassFile = toString (pkgs.writeText "password" "none");
@@ -43,6 +45,23 @@ in
 
     custom.virtualHosts.nextcloud = {
       onlyEnableTLS = true;
+    };
+
+    virtualisation.oci-containers.containers.collabora = {
+      image = "docker.io/collabora/code";
+      ports = [ "9980:9980" ];
+      autoStart = true;
+      environment = {
+        # This limits it to this NC instance AFAICT
+        aliasgroup1 = "https://${ncDomain}:443";
+        # Must disable SSL as it's behind a reverse proxy
+        extra_params = "--o:ssl.enable=false";
+      };
+    };
+
+    custom.virtualHosts.collaboracode = {
+      localPort = 9980;
+      inherit (config.custom.virtualHosts.nextcloud) onPrimaryDomain;
     };
   };
 }
