1668474cc7ad6367b646b487370d063210bad3bf
Atemu <atemu.main@gmail.com>
2024-06-10 20:10:27 +0200

    LYKOURGOS: init

---
+++ configs/LYKOURGOS/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../../common.nix
+    ../../hardware/FW16.nix
+  ];
+
+  networking.hostName = "LYKOURGOS";
+
+  custom.desktop.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
+  boot.initrd.systemd.enable = true;
+
+  services.sshd.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  programs.adb.enable = true;
+
+  custom.dnscrypt.enable = true;
+
+  custom.bootloader.choice = "systemd-boot";
+
+  custom.luks.autoDevices = 1;
+
+  custom.fs.enable = true;
+  custom.fs.btrfs.enable = true;
+  custom.fs.btrfs.newLayout = true;
+
+  system.stateVersion = "24.11";
+}
