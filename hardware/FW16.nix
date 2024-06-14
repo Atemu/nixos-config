b18d2562b882a17b9a50bc761ffd53d2a2a23c7a
Atemu <atemu.main@gmail.com>
2024-06-14 11:47:42 +0200

    FW16: enable fprintd
    It works so well!

--- hardware/FW16.nix
+++ hardware/FW16.nix
@@ -19,6 +19,8 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
 
+  services.fprintd.enable = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
