a6b341ccadd1d93e36c4febc323e3f599aaf9cb4
Atemu <atemu.main@gmail.com>
2024-06-24 01:58:51 +0200

    FW16: patch around excessive minimum backlight brightness

--- hardware/FW16.nix
+++ hardware/FW16.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   modulesPath,
   ...
 }:
@@ -17,6 +18,14 @@
     "sd_mod"
   ];
   boot.kernelModules = [ "kvm-amd" ];
+
+  custom.amdgpu.kernelModule.patches = [
+    (pkgs.fetchpatch2 {
+      url = "https://lore.kernel.org/lkml/20240610-amdgpu-min-backlight-quirk-v1-1-8459895a5b2a@weissschuh.net/raw";
+      hash = "sha256-tXxI+G9nNc+p4y8ITISe7EioCtETtePpeuCr+oWT/+4=";
+    })
+  ];
+
 
   services.fprintd.enable = true;
   services.fwupd.enable = true;
