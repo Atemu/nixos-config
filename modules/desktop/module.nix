56617349199ce9a767a120ab8dd270483cd78cfd
Atemu <git@atemu.net>
2024-11-07 03:32:48 +0100

    desktop: disable speechd

--- modules/desktop/module.nix
+++ modules/desktop/module.nix
@@ -242,5 +242,8 @@ in
     # Emacs likes to get itself stuck waiting on user input or some shit on the
     # rather explicit (kill-emacs) command. Let's not tolerate that.
     systemd.user.services.emacs.serviceConfig.TimeoutStopSec = "10s";
+
+    # No friggin 600MB mbrola!
+    services.speechd.enable = false;
   });
 }
