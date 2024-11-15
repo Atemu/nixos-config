60881ebd92f5d3f057f69182b09fde8150d947de
Atemu <git@atemu.net>
2024-11-16 00:40:36 +0100

    swap: use systemd-tmpfilesd to implement zswap settings

--- modules/swap/module.nix
+++ modules/swap/module.nix
@@ -37,34 +37,36 @@ in
       };
     }) enabledDevices;
 
-    boot.kernelParams = lib.mkIf this.zswap.enable [
-      "zswap.enabled=1"
+    systemd.tmpfiles.rules = lib.mkIf this.zswap.enable (
+      lib.mapAttrsToList (n: v: "w /sys/module/zswap/parameters/${n}  - - - - ${toString v}") {
+        enabled = true;
 
-      # I think with swap, you need speed first and foremost and compression
-      # efficiency is secondary. OTOH, in my testing, zstd is able to store the
-      # first 3 pages of random files in my closure in 1 page ~40% of the time
-      # vs. only ~10% with lz4 although the bulk is 2 pages in either case. I
-      # consider zswap a best-effort speed efficiency anyways though, so speed
-      # is more important I think.
-      "zswap.compressor=lz4"
+        # I think with swap, you need speed first and foremost and compression
+        # efficiency is secondary. OTOH, in my testing, zstd is able to store
+        # the first 3 pages of random files in my closure in 1 page ~40% of
+        # the time vs. only ~10% with lz4 although the bulk is 2 pages in
+        # either case. I consider zswap as a best-effort speed efficiency
+        # boost though, so speed is more important I think.
+        compressor = "lz4";
 
-      # AFAIK zsmalloc has memory fragmentation issues because it concatenates
-      # compressed pages with no regard for page boundaries. Reading a single
-      # page from compressed memory might require reading many pages. z3fold
-      # does not have such issues and a 3:1 compression ratio is good enough for
-      # me; the same point about me merely considering zswap an efficiency
-      # booster applies here.
-      "zswap.zpool=z3fold"
+        # AFAIK zsmalloc has memory fragmentation issues because it concatenates
+        # compressed pages with no regard for page boundaries. Reading a single
+        # page from compressed memory might require reading many pages. z3fold
+        # does not have such issues and a 3:1 compression ratio is good enough for
+        # me; the same point about me merely considering zswap an efficiency
+        # booster applies here.
+        zpool = "z3fold";
 
-      # This controls how much of the system RAM may be taken up by swap. On the
-      # one hand, you might think one wants this as high as possible but you
-      # must consider that at some point you do actually want stuff to be
-      # swapped out to disk as zswap is merely a stage between memory and swap
-      # that is cheaper than actual swap in terms of performance but still
-      # requires memory; memory that is still taken away from file-backed pages.
-      # Let's keep the default limit of 20% until I have formed a better opinion
-      # on this.
-      # "zswap.max_pool_percent=20"
-    ];
+        # This controls how much of the system RAM may be taken up by swap. On the
+        # one hand, you might think one wants this as high as possible but you
+        # must consider that at some point you do actually want stuff to be
+        # swapped out to disk as zswap is merely a stage between memory and swap
+        # that is cheaper than actual swap in terms of performance but still
+        # requires memory; memory that is still taken away from file-backed pages.
+        # Let's keep the default limit of 20% until I have formed a better opinion
+        # on this.
+        # max_pool_percent = "20";
+      }
+    );
   };
 }
