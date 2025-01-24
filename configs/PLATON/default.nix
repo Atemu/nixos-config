fba2c251c8413503163f6edeeda675544474a8cb
Atemu <git@atemu.net>
2025-01-24 07:21:55 +0100

    PLATON: drop unnecessary configuration

--- configs/PLATON/default.nix
+++ configs/PLATON/default.nix
@@ -19,18 +19,10 @@
   services.undervolt.enable = true;
   # It don't want the logspam, this is only needed because undervolt resets on boot
   systemd.timers.undervolt.enable = false;
-  # I don't exactly understand why but if undervolt needs to be both after and
-  # wanted by post-resume.target to be run on resume
-  # Being wanted by the multi-user.target makes undervolt run on boot and
-  # on generation switch in a live system
   systemd.services.undervolt.wantedBy = [ "post-resume.target" "multi-user.target" ];
   systemd.services.undervolt.after = [ "post-resume.target" ];
 
   networking.networkmanager.wifi.powersave = true;
-
-  systemd.services.ModemManager.wantedBy = [ "network.target" ];
-
-  virtualisation.docker.enable = true;
 
   custom.dnscrypt.enable = true;
 
