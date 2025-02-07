086bec7d5090d996119eb50da5a1c19525fe92e4
Atemu <git@atemu.net>
2025-02-07 16:43:50 +0100

    vm: fix eval
    It updated the mkIf...

--- modules/vm/module.nix
+++ modules/vm/module.nix
@@ -12,8 +12,8 @@ in
 {
   options.custom.vm.enable = lib.mkEnableOption "sensible defaults for use in a VM for testing";
 
-  config =
-    lib.mkIf this.enable {
+  config = lib.mkIf this.enable (
+    {
       users.users.atemu.initialHashedPassword = "";
       users.users.root.initialHashedPassword = "";
 
@@ -25,5 +25,6 @@ in
       # These options don't exist unless you are in the VM profile
       virtualisation.diskSize = 10 * 1024; # Create 10GiB disk by default
       virtualisation.memorySize = 4 * 1024; # 4GiB memory rather than the meager 1GiB
-    };
+    }
+  );
 }
