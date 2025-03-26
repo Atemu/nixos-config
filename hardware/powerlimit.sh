8520a0687734b3981c31bc2d9914d4241d5ef648
Atemu <git@atemu.net>
2025-03-26 14:41:53 +0100

    FW16: powerlimit: simplify using case statement

--- hardware/powerlimit.sh
+++ hardware/powerlimit.sh
@@ -2,19 +2,24 @@ limit="${1:-}"
 
 DEFAULT_LIMIT=80
 
-if [ -z "$limit" ]; then
-    current="$(ectool fwchargelimit)"
-    if [ "$current" = 100 ]; then
+case "$limit" in
+    "" | "get")
+        exec ectool fwchargelimit
+        ;;
+    "toggle")
+        current="$(ectool fwchargelimit)"
+        if [ "$current" = 100 ]; then
+            limit="$DEFAULT_LIMIT"
+        else
+            limit=100
+        fi
+        ;;
+    "on")
         limit="$DEFAULT_LIMIT"
-    else
+        ;;
+    "off")
         limit=100
-    fi
-elif [ "$limit" = "on" ]; then
-    limit="$DEFAULT_LIMIT"
-elif [ "$limit" = "off" ]; then
-    limit=100
-elif [ "$limit" = "get" ]; then
-    exec ectool fwchargelimit
-fi
+        ;;
+esac
 
 exec ectool fwchargelimit "$limit"
