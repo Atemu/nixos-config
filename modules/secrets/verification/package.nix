d487605c5b4a12dade7d3ab37cef8b55b2731613
Atemu <git@atemu.net>
2025-03-29 17:14:15 +0100

    init verification script

---
+++ modules/secrets/verification/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  rustPlatform,
+  runCommand,
+}:
+
+rustPlatform.buildRustPackage {
+  name = "secrets-verification";
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  src =
+    let
+      # Only copy a select few files, not the entire tree that includes Nix code
+      files = [
+        "src"
+        "Cargo.toml"
+        "Cargo.lock"
+      ];
+      commands =
+        [
+          "mkdir -p $out"
+        ]
+        ++ (map (file: "cp -r ${./${file}} $out/${file}") files);
+      script = lib.concatLines commands;
+    in
+    runCommand "source" { } script;
+
+  nativeBuildInputs = [
+    # rust-analyzer
+  ];
+}
---
+++ modules/secrets/verification/src/main.rs
@@ -0,0 +1,21 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Secret {
+    path: String, // TODO path type
+    user: String,
+    group: String,
+    mode: String, // TODO more abstract type?
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let spec_file = &args[1];
+
+    let contents = fs::read_to_string(spec_file)
+        .expect("Should have been able to read the file");
+    let secret: Secret = serde_json::from_str(&contents).unwrap();
+    println!("{:?}", secret);
+}
