a17a8a35e1b49c079292a1ed5796c763eda1b0c9
Atemu <git@atemu.net>
2025-03-29 21:52:33 +0100

    verify mode

--- modules/secrets/verification/src/main.rs
+++ modules/secrets/verification/src/main.rs
@@ -3,6 +3,9 @@ use std::env;
 use std::fs;
 use std::process;
 use std::path::Path;
+use std::os::unix::fs::MetadataExt;
+use thiserror::Error;
+// use std::fs::File;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Secret {
@@ -12,14 +15,33 @@ struct Secret {
     mode: String, // TODO more abstract type?
 }
 
+#[derive(Error, Debug)]
 enum VerificationError {
-    FileDoesNotExist(),
+    #[error("Secret does not exist")]
+    DoesNotExist(),
+    #[error("Secret has mode {found}, expected {expected}")]
+    ModeMismatch{
+        expected: u32,
+        found: u32,
+    },
 }
 fn verify(secret: &Secret) -> Result<(), VerificationError> {
     let path = Path::new(&secret.path);
 
     if !path.exists() {
-        return Err(VerificationError::FileDoesNotExist());
+        return Err(VerificationError::DoesNotExist());
+    }
+
+    let spec_mode = u32::from_str_radix(&secret.mode, 8).unwrap();
+    let metadata = fs::metadata(path).unwrap(); // Don't care about permission issues, this is supposed to be run as root
+    let mode = metadata.mode() & 0o777;
+    println!("{mode:o}");
+    println!("{spec_mode:o}");
+    if mode != spec_mode {
+        return Err(VerificationError::ModeMismatch{
+            expected: spec_mode,
+            found: mode,
+        });
     }
 
     Ok(())
@@ -32,13 +54,19 @@ fn main() {
     let contents = fs::read_to_string(spec_file)
         .expect("Should have been able to read the file");
     let spec: Vec<Secret> = serde_json::from_str(&contents).unwrap();
-    println!("{:?}", spec);
 
-    let mut result = spec.iter().map(verify);
+    let results = spec.iter().map(verify); // TODO HashMap
 
-    if result.any(|it| it.is_err()) {
+    let mut is_err = false;
+    for result in results {
+        if result.is_err() {
+            is_err = true;
+            println!("{}", result.err().unwrap());
+        }
+    }
+
+    if is_err {
         // TODO make fancy error messages using thiserror or something
-        println!("Bad!");
         process::exit(1);
     }
 }
