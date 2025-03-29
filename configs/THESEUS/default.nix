d4105f5084fbb2a0ffcc562212603dda7cb0aa14
Atemu <git@atemu.net>
2025-03-29 13:48:58 +0100

    THESEUS: enable replication for Users volume

--- configs/THESEUS/default.nix
+++ configs/THESEUS/default.nix
@@ -50,6 +50,23 @@
       "var/tmp"
     ];
   };
+  custom.replication.replications.Users = {
+    enable = true;
+    exclude = map (it: "atemu/${it}") [
+      ".cache"
+      ".local/share/Steam"
+      ".local/share/Trash"
+
+      "Repos/linux"
+      "Projects/robotnix/avd"
+      ".gradle"
+      ".emacs.d/.cache"
+      ".emacs.d/eln-cache"
+      ".cargo"
+      ".npm"
+      "go"
+    ];
+  };
 
   # Device name must be provided by hardware config for now
   custom.swap.devices.primary.enable = true;
