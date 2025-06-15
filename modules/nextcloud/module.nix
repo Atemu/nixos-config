5ad86da123e6a6754582308b20e511501f5c822f
Atemu <git@atemu.net>
2025-06-15 19:19:44 +0200

    nextcloud: update to 31

--- modules/nextcloud/module.nix
+++ modules/nextcloud/module.nix
@@ -34,7 +34,7 @@ in
   config = lib.mkIf this.enable {
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud31;
       hostName = ncDomain;
       https = true;
       config = {
