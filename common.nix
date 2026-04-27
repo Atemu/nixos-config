eb09f7c5cc8fdc09664fbe4ac3fe8a0b4596d926
Atemu <git@atemu.net>
2026-04-27 04:04:06 +0200

    common: lix_2_95

--- common.nix
+++ common.nix
@@ -163,7 +163,7 @@ in
   systemd.services.nix-daemon.environment.TMPDIR = "/var/tmp/";
 
   nix = {
-    package = if lib.trivial.release == "25.11" then pkgs.lixPackageSets.lix_2_94.lix else pkgs.lix;
+    package = if lib.trivial.release == "25.11" then pkgs.lixPackageSets.lix_2_95.lix else pkgs.lix;
 
     settings = {
       experimental-features = [
