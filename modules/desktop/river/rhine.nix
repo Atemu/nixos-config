377350a16c5c0252dd7018fe0973a72a1250a013
Atemu <git@atemu.net>
2026-07-13 16:51:06 +0200

    desktop: refactor xkeyboard overrides into options
    Need to use them in other modules too

--- modules/desktop/river/rhine.nix
+++ modules/desktop/river/rhine.nix
@@ -25,6 +25,13 @@ in
 {
   options.custom.desktop.river.rhine = {
     enable = lib.mkEnableOption "";
+    river.package =
+      (lib.mkPackageOption pkgs "river" { })
+      // lib.mkOption {
+        default = pkgs.river.override {
+          inherit (config.custom.desktop.keyboard.layout.packages) libxkbcommon;
+        };
+      };
   };
 
   config = lib.mkIf this.enable {
@@ -34,7 +41,7 @@ in
         binPath =
           # TODO write an abstraction around this
           pkgs.writeShellScriptBin "rhine" ''
-            exec ${lib.getExe pkgs.river} -c "${lib.getExe config.programs.uwsm.package} finalize WAYLAND_DISPLAY" "-no-xwayland" # handled via xwayland-satellite
+            exec ${lib.getExe this.river.package} -c "${lib.getExe config.programs.uwsm.package} finalize WAYLAND_DISPLAY" "-no-xwayland" # handled via xwayland-satellite
           ''
           |> lib.getExe;
         prettyName = "river-rhine";
