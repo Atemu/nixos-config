4f1cc04056c69aa4f7e7068c39f2b73062446bb7
Atemu <git@atemu.net>
2026-09-14 21:44:24 +0200

    HEPHAISTOS: enable rhine desktop

--- configs/HEPHAISTOS/default.nix
+++ configs/HEPHAISTOS/default.nix
@@ -13,6 +13,7 @@
 
   custom.desktop.enable = true;
   custom.desktop.hypr.enable = true;
+  custom.desktop.river.rhine.enable = true;
 
   custom.gaming.enable = true;
   custom.gaming.steam.bindmounts.enable = true;
